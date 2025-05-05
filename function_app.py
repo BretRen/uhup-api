364306f105651e264b663707c59f8785e313d2e8
BretRen <bretren2013@gmail.com>
2025-05-04 19:57:19 -0700

    Delete function_app.py

--- function_app.py
+++
@@ -1,9 +0,0 @@
-import azure.functions as func
-
-app = func.FunctionApp()
-
-@app.function_name(name="HttpTrigger1")
-@app.route(route="req")
-def main(req: func.HttpRequest) -> str:
-    user = req.params.get("user")
-    return f"Hello, {user}!"
